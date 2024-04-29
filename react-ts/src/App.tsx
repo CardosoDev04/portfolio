47dacd2d96e55bfad4f9037ad9f335d57c5c6941
50465 - Joao Cardoso <joaocardoson1@icloud.com>
2024-04-29 12:42:04 +0100

    W.I.P Portfolio Landing

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import GitHubWhite from '../src/assets/github_white.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+    function handleGithubClick() {
+        window.open("https://github.com/cardosodev04")
+    }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={"flex items-center justify-center border border-red-500 w-screen h-screen"}>
+        <div className={"flex items-center justify-center flex-col w-3/4 h-3/4 rounded-2xl bg-black"}>
+            <h1 className={"text-3xl text-white font-semibold"}>You've reached my portfolio</h1>
+            <div className={"flex flex-col text-center mt-5"}>
+            <span className={"text-xl text-white"}>
+                You're probably wondering, where are all the projects and valuable info about me and my work?
+            </span>
+            <span className={"text-xl text-white"}>
+                Well... I'm still working on it :(
+            </span>
+                <span className={"text-xl text-white"}>
+                In the meantime, you can check out my github profile.
+            </span>
+            </div>
+            <img alt={"github"} src={GitHubWhite} className={"w-12 h-12 mt-5 cursor-pointer"} onClick={handleGithubClick}/>
+        </div>
+    </div>
   )
 }
 
